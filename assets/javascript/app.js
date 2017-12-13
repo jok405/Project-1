f8173cbc3f3f5db05208ebf25752272693c470f5
Jared <Jared@Littles-MacBook-Pro.local>
2017-12-12 21:34:28 -0500

    adding js and script tags

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+
+
+var restaurantArray = [];
+var activityArray = ["Movie", "Concert", "Beer Tasting", "Bike Ride", "Hike"];
+var randomNum;
+
+function randomNum {
+	randomNum = Math.floor(Math.random() * 4);
+}
+
+function fillRestaurant {
+	for (var i = 0; i < apiArray.length; i++) {
+		restaurantArray.push(restaurant.val());
+	}
+}
+
+console.log(activityArray[randomNum]);
+console.log(restaurantArray[randomNum]);
+
+$(document).ready(function(){
+
+$(#wheelSpin).on("click", function(event)) {
+
+}
+
+}
