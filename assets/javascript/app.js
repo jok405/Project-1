8b88c6ddbeee8ace3b5b195e8c4dc737569d4d86
Jared <Jared@Littles-MacBook-Pro.local>
2017-12-18 15:03:58 -0500

    working product

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,62 @@
-
-
-//background animation experiment
-
-	
-	
-	
-//DOCUMENT READY
-	
-//set default degree (360*5)
-var degree = 1800;
-//number of clicks = 0
+var movieArray = [];
+var data = [];
+var apikey = "m8zfezvnzgt2uda46zuqe9e7";
+var baseUrl = "http://data.tmsapi.com/v1.1";
+var showtimesUrl = baseUrl + '/movies/showings';
+var zipCode = "23222";
+var d = new Date();
+var today = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate();
+var randomRest;
+var randomMov;
 var clicks = 0;
+var dinnerButton = false;
+var activityButton = false;
+var bothButton = false;
+var degree = 1800;
+var restArray = []; 
+var queryURL = "https://developers.zomato.com/api/v2.1/cities?q=richmond";
+var queryURLl = "0356c6221d55cd4bfb3231fee709ccec";
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://developers.zomato.com/api/v2.1/search?entity_id=1219&entity_type=city&q=restaurants&count=20",
+  "method": "GET",
+  "headers": {
+    "user-key": "0356c6221d55cd4bfb3231fee709ccec",
+  }
+};
 
-$(document).ready(function(){
-	
-	/*WHEEL SPIN FUNCTION*/
-	$('#spin').click(function(){
+
+function randomRestaurant() {
+  		randomRest = restArray[Math.floor(Math.random() * 19)];
+  	}
+function display() {
+	$("#snark").text("Like it or not, you are going to ...")
+	if (dinnerButton == true) {
+		$("#rest-txt").text("Go eat at " + randomRest);
+		console.log(dinnerButton);
+		dinnerButton = false;
+	} 
+	if (activityButton == true) {
+		$("#rest-txt").text("Watch " + randomMov);
+		activityButton = false;
+	}
+	if (bothButton == true) {
+		$("#rest-txt").text("Watch " + randomMov + " and eat at " + randomRest);
+		bothButton = false;
+	}
+};
+function randomMovie() {
+	//randomMov = "Red Dawn";
+	randomMov = movieArray[Math.floor(Math.random() * data.length)]; //////////////////////////////////data.length
+	console.log('randomMovieFunction')
+}	
+function timer() {
+			setTimeout(display, 6000)
+		}
+function spin () {
+		timer();
+		$("#rest-txt").text("");
 		
 		//add 1 every click
 		clicks ++;
@@ -27,9 +68,6 @@ $(document).ready(function(){
 		var extraDegree = Math.floor(Math.random() * (360 - 1 + 1)) + 1;
 		totalDegree = newDegree+extraDegree;
 		
-		/*let's make the spin btn to tilt every
-		time the edge of the section hits 
-		the indicator*/
 		$('#wheel .sec').each(function(){
 			var t = $(this);
 			var noY = 0;
@@ -67,14 +105,54 @@ $(document).ready(function(){
 			noY = t.offset().top;
 			
 		});
-	});
+	}
 
-//background animation experiment
 
-	
-	
-	
-});//DOCUMENT READY
-	
+$.ajax(settings).done(function (response) {
+  	for (var i = 0; i < 19; i++) {
+    	restArray.push(response.restaurants[i].restaurant.name);
+  		} 
+	 });       
+
+$.ajax({
+    url: showtimesUrl,
+    data: { startDate: today,
+    zip: zipCode,
+    jsonp: "dataHandler",
+    api_key: apikey
+    },          
+    dataType: "jsonp",
+    });
+         //});
+function dataHandler(data) {
+    for (var i = 0; i < data.length; i++) {
+        movieArray.push(data[i].title);
+        	}
+        }
+   
+        //Here is the random movie pulled from the Movie Array 
+    //randomMov = movieArray[Math.floor(Math.random() * data.length)];
+	//randomMovie()
+$(document).ready(function() {
+//randomMov = "Red Dawn";
+    $(document).on('click', '#activity', function(spin) {
+    	activityButton = true;//$("#rest-txt").text(randomMov);
+  		console.log(randomMov)
+  		randomMovie();
+		});
+	$(document).on('click', '#both', function(spin) {
+		bothButton = true;
+		randomRestaurant ();
+		randomMovie();//$("#rest-txt").text("Watch " + randomMov + " and eat at " + randomRest);
+  		console.log(randomMov);
+		});
+	$('#spin').click(function(){
+		spin();
+		});
+	$(document).on('click', '#dinner', function(spin) {
+	 	dinnerButton = true;
+	 	randomRestaurant ();//$("#rest-txt").text(randomRest);
+	 	});
+});
 
 
--- assets/javascript/jared.js
+++ assets/javascript/jared.js
@@ -1,90 +1,79 @@
 
 
   
-// Zomato API that pulls restaraunt information 
-var queryURL = "https://developers.zomato.com/api/v2.1/cities?q=richmond";
+// // Zomato API that pulls restaraunt information 
+// var queryURL = "https://developers.zomato.com/api/v2.1/cities?q=richmond";
 
-var queryURLl = "0356c6221d55cd4bfb3231fee709ccec";
+// var queryURLl = "0356c6221d55cd4bfb3231fee709ccec";
 
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://developers.zomato.com/api/v2.1/search?entity_id=1219&entity_type=city&q=restaurants&count=20",
-  "method": "GET",
-  "headers": {
-    "user-key": "0356c6221d55cd4bfb3231fee709ccec",
-  }
-};
-// array of our restaraunts
-var restArray = [];
-// ajax call gives us 20 local restaraunts
-$.ajax(settings).done(function (response) {
-  for (var i = 0; i < 19; i++) {
-    // console.log(response.restaurants[i].restaurant.name);
-    // Pushes our 20 restaraunts into our restArray array 
-    restArray.push(response.restaurants[i].restaurant.name);
-  }
-
-  // console.log(restArray);
-  // Randomizes through our array and pulls one option
-  var randomRest = restArray[Math.floor(Math.random() * 19)];
- // here is our radom restaraunt, need to push to a card 
-console.log(randomRest);
-$("#rest-txt").text(randomRest);
-
-
-$(document).on('click', '#dinner', function(spin) {
- 
-  console.log(randomRest);
-});
-
-$(document).on('click', '#both', function(spin) {
-  console.log(randomRest);
-  
-});
-});
-// Here is our movie API that pulls our movie infor based on zip code
- // construct the url with parameter values
-         var apikey = "m8zfezvnzgt2uda46zuqe9e7";
-         var baseUrl = "http://data.tmsapi.com/v1.1";
-         var showtimesUrl = baseUrl + '/movies/showings';
-         var zipCode = "23222";
-         var d = new Date();
-         var today = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate();
+// var settings = {
+//   "async": true,
+//   "crossDomain": true,
+//   "url": "https://developers.zomato.com/api/v2.1/search?entity_id=1219&entity_type=city&q=restaurants&count=20",
+//   "method": "GET",
+//   "headers": {
+//     "user-key": "0356c6221d55cd4bfb3231fee709ccec",
+//   }
+// };
+// // array of our restaraunts
+// var restArray = [];
+// // ajax call gives us 20 local restaraunts
+// $.ajax(settings).done(function (response) {
+//   for (var i = 0; i < 19; i++) {
+//     // console.log(response.restaurants[i].restaurant.name);
+//     // Pushes our 20 restaraunts into our restArray array 
+//     restArray.push(response.restaurants[i].restaurant.name);
+//     }
+//     var randomRest = restArray[Math.floor(Math.random() * 19)];
+//     console.log(randomRest);
+//     $("#rest-txt").text(randomRest);
+//     $(document).on('click', '#dinner', function(spin) {
+//     console.log(randomRest);
+//     });
+//     $(document).on('click', '#both', function(spin) {
+//     console.log(randomRest);
+//     });
+//   });
+//          var apikey = "m8zfezvnzgt2uda46zuqe9e7";
+//          var baseUrl = "http://data.tmsapi.com/v1.1";
+//          var showtimesUrl = baseUrl + '/movies/showings';
+//          var zipCode = "23222";
+//          var d = new Date();
+//          var today = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate();
           
-         $(document).ready(function() {
+//          $(document).ready(function() {
           
-           // send off the query
-           $.ajax({
-            url: showtimesUrl,
-            data: { startDate: today,
-            zip: zipCode,
-            jsonp: "dataHandler",
-            api_key: apikey
-            },          
-            dataType: "jsonp",
-           });
-         });
+//            // send off the query
+//            $.ajax({
+//             url: showtimesUrl,
+//             data: { startDate: today,
+//             zip: zipCode,
+//             jsonp: "dataHandler",
+//             api_key: apikey
+//             },          
+//             dataType: "jsonp",
+//            });
+//          });
 
-        var movieArray = [];
+//         var movieArray = [];
 
-         // callback to handle the results
-           function dataHandler(data) {
-           // $(document.body).append('<p>Found ' + data.length + ' movies showing within 5 miles of ' + zipCode+':</p>');
-           // var movies = data.hits;
-           for (var i = 0; i < data.length; i++) {
-             console.log(data[i]);
-            movieArray.push(data[i].title);
-           }
-           // Here is the random movie pulled from the Movie Array 
-           var randomMov = movieArray[Math.floor(Math.random() * data.length)];
-           console.log(randomMov);
-           $("#act-txt").text(randomMov);
-           $(document).on('click', '#activity', function(spin) {
-  console.log(randomMov)
-});
-$(document).on('click', '#both', function(spin) {
+//          // callback to handle the results
+//            function dataHandler(data) {
+//            // $(document.body).append('<p>Found ' + data.length + ' movies showing within 5 miles of ' + zipCode+':</p>');
+//            // var movies = data.hits;
+//            for (var i = 0; i < data.length; i++) {
+//              console.log(data[i]);
+//             movieArray.push(data[i].title);
+//            }
+//            // Here is the random movie pulled from the Movie Array 
+//            var randomMov = movieArray[Math.floor(Math.random() * data.length)];
+//            console.log(randomMov);
+//            $("#act-txt").text(randomMov);
+//            $(document).on('click', '#activity', function(spin) {
+//   console.log(randomMov)
+// });
+// $(document).on('click', '#both', function(spin) {
   
-  console.log(randomMov);
-});
-         }
+//   console.log(randomMov);
+// });
+//          }
